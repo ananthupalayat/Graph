1971b26c53044893371d856deb388afea9ef3feb
ananthupalayat <ananthupalayat85@gmail.com>
2022-02-05 11:12:56 +0530

    Initial commit

---
+++ Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
